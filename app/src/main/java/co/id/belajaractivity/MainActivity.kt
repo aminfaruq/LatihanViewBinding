c2b6521a04f172149800a0f07badd96c4b94bad6
Faruk Amin <aminfaruk.fa@gmail.com>
2020-05-26 11:55:25 +0800

    Initial commit

---
+++ app/src/main/java/co/id/belajaractivity/MainActivity.kt
@@ -0,0 +1,79 @@
+package co.id.belajaractivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import co.id.belajaractivity.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var edtLength: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+
+    companion object {
+        private const val STATE_RESULT = "state_result"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        edtWidth = binding.edtWidth
+        edtHeight = binding.edtHeight
+        edtLength = binding.edtLength
+        btnCalculate = binding.btnCalculate
+        tvResult = binding.tvResult
+        btnCalculate.setOnClickListener(this)
+
+        if (savedInstanceState != null) {
+            val result = savedInstanceState.getString(STATE_RESULT) as String
+            tvResult.text = result
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT, tvResult.text.toString())
+
+    }
+
+    override fun onClick(p0: View?) {
+        val inputLength = edtLength.text.toString().trim()
+        val inputWidth = edtWidth.text.toString().trim()
+        val inputHeight = edtHeight.text.toString().trim()
+
+        var isEmptyFields = false
+
+        if (inputLength.isEmpty()) {
+            isEmptyFields = true
+            edtLength.error = "Fields tidak boleh kosong"
+        }
+
+        if (inputWidth.isEmpty()) {
+            isEmptyFields = true
+            edtWidth.error = "Fields tidak boleh kosong"
+
+        }
+
+        if (inputHeight.isEmpty()) {
+            isEmptyFields = true
+            edtHeight.error = "Fields tidak boleh kosong"
+        }
+
+        if (!isEmptyFields) {
+            val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
+            tvResult.text = volume.toString()
+        }
+
+    }
+}
